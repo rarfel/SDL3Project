743201da0e027c334c58c5d62b50dec11b28df91
rarfel <faelmvitor@gmail.com>
2025-04-28 19:37:02 -0300

    Uploading to github

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include "../vendor/SDL/include/SDL3/SDL.h"
 
-int main(int argc, char* argv[])
+int main()
 {
-    SDL_Log("%s", "Hello World!");
+    int WIDTH = 720; 
+    int HEIGHT = 480;
+    auto window = SDL_CreateWindow("Teste",WIDTH,HEIGHT,SDL_WINDOW_RESIZABLE);
+
+    if(!window)
+        SDL_LogCritical(SDL_LogCategory::SDL_LOG_CATEGORY_ERROR, SDL_GetError());
+
+    bool isPlaying = true;
+    while(isPlaying)
+    {
+        SDL_Event event;
+        while(SDL_PollEvent(&event))
+            if(event.type == SDL_EventType::SDL_EVENT_QUIT)
+                isPlaying = false;
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_QuitSubSystem(SDL_INIT_VIDEO);
     return 0;
 }
